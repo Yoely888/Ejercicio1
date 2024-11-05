ae9f9e365a04f0952622954281734bcb8d56f0ac
Yoely888 <fuentes888github@gmail.com>
2024-11-05 00:41:27 -0600

    Inicio

---
+++ src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio1;
+
+/**
+ *
+ * @author 14-cf2074
+ */
+public class Ejercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola, soy Keysi Fuentes");
+        System.out.println("Un gusto saludarle");
+    }
+}
